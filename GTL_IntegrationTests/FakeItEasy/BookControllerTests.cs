fda42bc3cba17f2c9a4150ca31533f61c9aabf3d
DESKTOP-JRQFD6G\inter <jonakirke94@gmail.com>
2019-02-13 19:47:15 +0100

    added fakeiteasy example and mocked review in moq

---
+++ GTL_IntegrationTests/FakeItEasy/BookControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using FakeItEasy;
+using GTL_Integration.Controllers;
+using GTL_Integration.Library;
+using GTL_Integration.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GTL_IntegrationTests.FakeItEasy
+{
+    public class BookControllerTests
+    {
+        [Theory]
+        [InlineData(1, "LOTR", "A great story about a ring")]
+        public void Will_Return_One_Review(int reviewId, string title, string body)
+        {
+            var fakeReview = A.Fake<Review>();
+            var bookServiceStub = A.Fake<IBookProvider>();
+
+            A.CallTo(() => bookServiceStub.GetBookReview(A<int>._)).Returns(fakeReview);
+         
+
+            var sut = new BookController(bookServiceStub);
+
+            var review = sut.GetBookReview(reviewId);
+
+            var manager = Fake.GetFakeManager(review);
+            Assert.Equal(typeof(Review), manager.FakeObjectType);
+            A.CallTo(() => bookServiceStub.GetBookReview(A<int>._)).MustHaveHappened();
+
+        }
+    }
+}
---
+++ GTL_IntegrationTests/Moq/BookControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using Castle.DynamicProxy;
+using GTL_Integration.Controllers;
+using GTL_Integration.Library;
+using GTL_Integration.Models;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GTL_IntegrationTests.Moq
+{
+    public class BookControllerTests
+    {
+        [Theory]
+        [InlineData(1, "LOTR", "A great story about a ring")]
+        public void Will_Return_One_Review(int reviewId, string title, string body)
+        {
+            var fakeReview = new Mock<Review>();
+            fakeReview.Object.Id = reviewId;
+            fakeReview.Object.Title = title;
+            fakeReview.Object.Body = body;
+
+            var bookServiceStub = new Mock<IBookProvider>();
+
+            bookServiceStub.Setup(x => x.GetBookReview(It.IsAny<int>())).Returns(() => fakeReview.Object);
+
+            var sut = new BookController(bookServiceStub.Object);
+
+            var review = sut.GetBookReview(reviewId);
+
+            Assert.Equal(reviewId, review.Id);
+            Assert.Equal(title, review.Title);
+            Assert.Equal(body, review.Body);
+            bookServiceStub.Verify(x => x.GetBookReview(It.IsAny<int>()), Times.Once());
+        }
+    }
+}
